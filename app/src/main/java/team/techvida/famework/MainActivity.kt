8f03a97526c944591a4c3ffbe127da55cbd279c7
AvidA <AtrasVidA@gmail.com>
2022-01-27 18:30:51 +0330

    init project

---
+++ app/src/main/java/team/techvida/famework/MainActivity.kt
@@ -0,0 +1,38 @@
+package team.techvida.famework
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import team.techvida.famework.ui.theme.FameworkTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FameworkTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FameworkTheme {
+        Greeting("Android")
+    }
+}
